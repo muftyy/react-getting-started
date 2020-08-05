3b8e48fc61535e5edefc713d0f0cbddaf895530b
muftyy <41176161+faddalibrahim@users.noreply.github.com>
2020-08-05 03:13:12 +0000

    just created my first component and nested it, yaaayyy

---
+++ src/Ninjas.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class Ninjas extends Component{
+  render(){
+    return(
+      <div className="ninjas">
+        <p>Name: Ryu</p>
+        <p>Age: 21</p>
+        <p>Belt: Orange</p>
+      </div>
+    )
+  }
+}
+
+export default Ninjas;
