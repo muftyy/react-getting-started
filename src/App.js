b499f88fe0c81659f4d8dc2ca94cfae98728250e
muftyy <41176161+faddalibrahim@users.noreply.github.com>
2020-08-05 03:47:03 +0000

    function and class-based components whooosh

--- src/App.js
+++ src/App.js
@@ -2,11 +2,19 @@ import React, { Component } from 'react';
 import Ninjas from './Ninjas';
 
 class App extends Component{
+  state = {
+    ninjas : [
+      {name: 'Ryu', age: 30, belt: 'pink', id: 1},
+      {name: 'Mario', age: 66, belt: 'red', id: 2},
+      {name: 'Luigi', age: 34, belt: 'blue', id: 3}
+    ]
+  }
+
   render(){
     return(
       <div>
         i am gonna print a component below
-        <Ninjas name="Mufti" age="27" belt="violet"/>
+        <Ninjas ninjas={this.state.ninjas}/>
       </div>
     )
   }
--- src/Ninjas.js
+++ src/Ninjas.js
@@ -1,16 +1,11 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class Ninjas extends Component{
-  render(){
-      const { name, age, belt } = this.props;
-    return(
-      <div className="ninjas">
-        <p>Name: {name}</p>
-        <p>Age: {age}</p>
-        <p>Belt: {belt}</p>
-      </div>
+const Ninjas = ({ninjas}) => {
+      return(
+        <div className="ninjas">
+          { ninjas.map(ninja => <p key={ninja.id}>{ninja.name} {ninja.age} {ninja.belt}</p>) }
+        </div>
     )
-  }
 }
 
 export default Ninjas;
