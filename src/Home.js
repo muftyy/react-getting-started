fd6ce4a436e7994fb31ad49b4aff4da898e71740
muftyy <41176161+faddalibrahim@users.noreply.github.com>
2020-08-07 11:56:16 +0000

    installed materialize css and ouput posts from axios

--- src/Home.js
+++ src/Home.js
@@ -1,14 +1,49 @@
-import React from 'react';
+import React, { Component } from 'react';
+import Axios from 'axios';
+import 'materialize-css/dist/css/materialize.min.css'
 
+class Home extends Component{
+  state = {
+    posts: []
+  }
 
-const Home = () => {
-    return(
-      <div className="container">
-        <h4 className="center">Home</h4>
-        <p>this is the home page you mother fuckers</p>
+  componentDidMount(){
+    Axios.get('https://jsonplaceholder.typicode.com/posts')
+    .then(res => {
+        this.setState({
+          posts: res.data.slice(0,10)
+        })
+    })
+  }
+
+  render(){
+    const { posts } = this.state;
+    const postList = posts.length ? (
+      posts.map(post => {
+        return(
+          <div className="post card" key={post.id}>
+            <div className="card-content">
+              <div className="card-title">
+                <span>{post.title}</span>
+                <p>{post.body}</p>
+              </div>
+            </div>
+          </div>
+        )
+      })
+    ) : (
+      <div>
+        Posts Loading...
       </div>
     )
 
+    return(
+      <div className="container">
+        <h4 className="center">Home</h4>
+        {postList}
+      </div>
+    )
+  }
 }
 
 export default Home;
--- src/Navbar.js
+++ src/Navbar.js
@@ -4,13 +4,17 @@ import {Link, NavLink} from 'react-router-dom';
 
 const Navbar = () => {
     return(
-        <nav className="nav-wrapper red darken-3">
+        <nav className="nav-wrapper blue darken-3">
             <div className="container">
                 <a href="/" className="brand-logo">Poke' Times</a>
                 <ul className="right">
                     <li>
                         <Link to="/">Home</Link>
+                    </li>
+                    <li>
                         <Link to="/about">About</Link>
+                    </li>
+                    <li>
                         <Link to="/contact">Contact</Link>
                     </li>
                 </ul>
