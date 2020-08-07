ec5635b12a19e793735de19eb8c5a4f31b67f5a1
muftyy <41176161+faddalibrahim@users.noreply.github.com>
2020-08-07 13:17:32 +0000

    blog project complete

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import Navbar from './Navbar';
-import {BrowserRouter as Router, Route} from 'react-router-dom';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import About from './About';
 import Contact from './Contact';
 import Home from './Home';
@@ -13,10 +13,12 @@ class App extends Component{
       <Router>
         <div className="App">
           <Navbar/>
-          <Route exact path="/" component={Home}/>
-          <Route path="/about" component={About}/>
-          <Route path="/contact" component={Contact}/>
-          <Route path="/:post_id" component={Post}/>
+          <Switch>
+            <Route exact path="/" component={Home}/>
+            <Route path="/about" component={About}/>
+            <Route path="/contact" component={Contact}/>
+            <Route path="/:post_id" component={Post}/>
+          </Switch>
         </div>
       </Router>
     )
--- src/Home.js
+++ src/Home.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import Axios from 'axios';
 import 'materialize-css/dist/css/materialize.min.css';
 import { Link } from 'react-router-dom';
+import ball from './pokeball.png'
 
 class Home extends Component{
   state = {
@@ -23,6 +24,7 @@ class Home extends Component{
       posts.map(post => {
         return(
           <div className="post card" key={post.id}>
+          <img src={ball}/>
             <div className="card-content">
               <div className="card-title">
                 {/* <span>{post.title}</span> */}
@@ -40,7 +42,7 @@ class Home extends Component{
     )
 
     return(
-      <div className="container">
+      <div className="container home">
         <h4 className="center">Home</h4>
         {postList}
       </div>
